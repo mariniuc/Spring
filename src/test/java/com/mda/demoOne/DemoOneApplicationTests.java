39b52d9e37273b0975f711bfbe7c1f3920be499f
dan <dan_mariniuc@yahoo.com>
2019-02-14 14:13:15 +0200

    test commit

---
+++ src/test/java/com/mda/demoOne/DemoOneApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mda.demoOne;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
